c1777d61ecdabf66723ac80ee73033aff2aee147
Giovanni <leviatandemon@gmail.com>
2013-03-28 14:05:55 -0500

    Español-ingles DB
    cambio de la base de datos de español a ingles
    falta tareas y el atributo especializacion

--- sln/DB/2ProceduresCreate.sql
+++ sln/DB/2ProceduresCreate.sql
@@ -7,95 +7,95 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE PROCEDURE [dbo].[UserGet] 
-	@Documento int
+	@Document int
 AS
 BEGIN
-	SELECT * FROM USUARIO 
-	WHERE Documento = @Documento
+	SELECT * FROM [USER] 
+	WHERE Document = @Document
 END
 
 GO
 
-CREATE PROCEDURE [dbo].[CreateUser] 
-	@Documento int,
-	@Nombre varchar(50),
+CREATE PROCEDURE [dbo].[UserCreate] 
+	@Document int,
+	@Name varchar(50),
 	@Password varchar(50),
 	@ID_Rol int,
-	@ID_Localizacion int
+	@ID_Localization int
 AS
 BEGIN
-	INSERT INTO [SistemaTecnicos].[dbo].[USUARIO]
-           ([Documento]
-           ,[Nombre]
+	INSERT INTO [dbo].[[USER]]
+           ([Document]
+           ,[Name]
            ,[Password]
            ,[ID_Rol]
-           ,[ID_Localizacion])
+           ,[ID_Localization])
 	VALUES
-           (@Documento
-           ,@Nombre
+           (@Document
+           ,@Name
            ,@Password
            ,@ID_Rol
-           ,@ID_Localizacion)
-	SELECT Documento FROM USUARIO WHERE Documento = @Documento
+           ,@ID_Localization)
+	SELECT Document FROM [USER] WHERE Document = @Document
 END
 
 GO
 
-CREATE PROCEDURE [dbo].[UpdateUser] 
-	@Documento int,
-	@Nombre varchar(50),
+CREATE PROCEDURE [dbo].[UserUpdate] 
+	@Document int,
+	@Name varchar(50),
 	@Password varchar(50),
 	@ID_Rol int,
-	@ID_Localizacion int
+	@ID_Localization int
 AS
 BEGIN
-	UPDATE [SistemaTecnicos].[dbo].[USUARIO]
-   SET [Nombre] = @Nombre
+	UPDATE [dbo].[[USER]]
+   SET [Name] = @Name
       ,[Password] = @Password
       ,[ID_Rol] = @ID_Rol
-      ,[ID_Localizacion] = @ID_Localizacion
- WHERE Documento = @Documento
- SELECT Documento FROM USUARIO WHERE Documento = @Documento
+      ,[ID_Localization] = @ID_Localization
+ WHERE Document = @Document
+ SELECT Document FROM [USER] WHERE Document = @Document
 END
 
 GO
 
 CREATE PROCEDURE [dbo].[AgentGet] 
-	@Documento int
+	@Document int
 AS
 BEGIN
-	SELECT * FROM AGENTE
-	WHERE Documento = @Documento
+	SELECT * FROM AGENT
+	WHERE Document = @Document
 END
 
 GO
 
-CREATE PROCEDURE [dbo].[CreateAgent] 
-	@Documento int,
+CREATE PROCEDURE [dbo].[AgentCreate] 
+	@Document int,
 	@Especializacion varchar(50),
-	@ID_Coordinador int
+	@ID_Manager int
 AS
 BEGIN
-	INSERT INTO [SistemaTecnicos].[dbo].[AGENTE]
-           ([Documento]
+	INSERT INTO [SistemaTecnicos].[dbo].[AGENT]
+           ([Document]
            ,[Especializacion]
-           ,[ID_Coordinador])
+           ,[ID_Manager])
      VALUES
-           (@Documento
+           (@Document
            ,@Especializacion
-           ,@ID_Coordinador)
-    SELECT Documento FROM AGENTE WHERE Documento = @Documento
+           ,@ID_Manager)
+    SELECT Document FROM AGENT WHERE Document = @Document
 END
 GO
 
-CREATE PROCEDURE [dbo].[UpdateAgent] 
-	@Documento int,
+CREATE PROCEDURE [dbo].[AgentUpdate] 
+	@Document int,
 	@Especializacion varchar(50),
-	@ID_Coordinador int
+	@ID_Manager int
 AS
 BEGIN
-	UPDATE [SistemaTecnicos].[dbo].[AGENTE]
+	UPDATE [dbo].[AGENT]
    SET [Especializacion] = Especializacion
-      ,[ID_Coordinador] = ID_Coordinador
- WHERE Documento = @Documento
+      ,[ID_Manager] = ID_Manager
+ WHERE Document = @Document
 END
