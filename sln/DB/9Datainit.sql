6a9dbdddce101d13015e04a3a11381a72e4dc21c
jaserranoj <jaserranoj@unal.edu.co>
2013-05-26 13:46:36 -0500

    Script Estados de Tareas

---
+++ sln/DB/9Datainit.sql
@@ -0,0 +1,44 @@
+INSERT INTO [SistemaTecnicos].[dbo].[MANAGER]
+           ([Document])
+     VALUES
+           (1)
+GO
+
+
+INSERT INTO [SistemaTecnicos].[dbo].[STATE]
+           ([ID]
+           ,[Name])
+     VALUES
+           (1
+           ,'Pendiente')
+GO
+INSERT INTO [SistemaTecnicos].[dbo].[STATE]
+           ([ID]
+           ,[Name])
+     VALUES
+           (2
+           ,'En Curso')
+GO
+INSERT INTO [SistemaTecnicos].[dbo].[STATE]
+           ([ID]
+           ,[Name])
+     VALUES
+           (3
+           ,'Por Aprobar')
+GO
+INSERT INTO [SistemaTecnicos].[dbo].[STATE]
+           ([ID]
+           ,[Name])
+     VALUES
+           (4
+           ,'Aprobado')
+GO
+INSERT INTO [SistemaTecnicos].[dbo].[STATE]
+           ([ID]
+           ,[Name])
+     VALUES
+           (5
+           ,'Cancelada')
+GO
+
+
