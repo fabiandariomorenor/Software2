c5afdd9762934eace044471081326cad60f8ce38
Julian Cardenas <julicardenas2010@gmail.com>
2013-06-19 19:53:14 -0500

    BusyAgent piloto
    procedimiento busyagent no oficial

---
+++ sln/DB/Sprint 2/ProceduresStatisticsAgent.sql
@@ -0,0 +1,22 @@
+﻿USE [SistemaTecnicos]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[BusyAgent]
+	@Datetime varchar(20)
+AS
+BEGIN
+	SELECT [USER].Name, [AGENT].Document
+	FROM [TASK] 
+	JOIN Agent on Agent.Document=[TASK].ID_Agent
+	JOIN [USER] on [USER].Document=Agent.Document
+	JOIN [STATE] on [STATE].Id=[TASK].ID_State
+	WHERE ID_State=2 AND InitDate>= CONVERT(datetime, @Datetime,131)
+END
+GO
+
